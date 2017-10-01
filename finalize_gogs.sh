d93bf34c4a139400886121e81b7130ed6298c362
root <ansgar.dahlen@googlemail.com>
2017-10-01 21:21:20 +0000

    change docker container names from git to gogs

--- finalize_gogs.sh
+++ finalize_gogs.sh
@@ -2,4 +2,4 @@
 
 source ./gogs.conf
 
-docker exec --user git dc_git bash -c "export USER=git; /app/gogs/gogs admin create-user --name $GOGS_ADMIN --password $GOGS_PASS --email $GOGS_ADMIN_MAIL --admin true"
+docker exec --user git dc_gogs bash -c "export USER=git; /app/gogs/gogs admin create-user --name $GOGS_ADMIN --password $GOGS_PASS --email $GOGS_ADMIN_MAIL --admin true"
