25e63ae171fcb6f4f464d4f500c70ab8a551bc0f
Ansgar Dahlen <ansgar.dahlen@googlemail.com>
2019-05-01 09:46:13 +0200

    bugfix admin creation failes because wrong variable at mail-address

--- finalize.sh
+++ finalize.sh
@@ -2,4 +2,5 @@
 
 source ./gogs.conf
 
-docker exec --user git dc_gogs bash -c "export USER=git; /app/gogs/gogs admin create-user --name $GOGS_ADMIN --password $GOGS_PASS --email $GOGS_ADMIN_MAIL --admin true"
+#docker exec --user git dc_gogs bash -c "export USER=git; /app/gogs/gogs admin create-user --name $GOGS_ADMIN --password $GOGS_PASS --email $GOGS_ADMIN_MAIL --admin true"
+docker exec --user git dc_gogs bash -c "export USER=git; /app/gogs/gogs admin create-user --name $GOGS_ADMIN --password $GOGS_PASS --email $ADMIN_MAIL --admin "
